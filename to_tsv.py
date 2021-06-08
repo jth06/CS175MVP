c5529703d9ced381a4e146bae75065f4910d8f9c
Charles <13723004+charzho@users.noreply.github.com>
2021-06-08 13:36:22 -0700

    Update to_tsv.py

--- to_tsv.py
+++ to_tsv.py
@@ -4,14 +4,15 @@ from os import listdir
 
 path = ".\\Twitch_data"
 
-header = ['stream_ID', 'current_views', 'time_created', 'game_name', 'broadcaster_id', 'broadcaster_name', 'delay_setting', 'follower_number', 'partner_status', 'broadcaster_language', 'total_views', 'language', 'sign_up_time', 'bitrate', 'source_resolution']
+#header = ['stream_ID', 'current_views', 'time_created', 'game_name', 'broadcaster_id', 'broadcaster_name', 'delay_setting', 'follower_number', 'partner_status', 'broadcaster_language', 'total_views', 'language', 'sign_up_time', 'bitrate', 'source_resolution']
+header = ['current_views', 'time_created', 'game_name', 'broadcaster_id', 'broadcaster_name', 'delay_setting', 'follower_number', 'partner_status', 'broadcaster_language', 'total_views', 'language', 'sign_up_time', 'bitrate', 'source_resolution', 'empty']
 
 dfs = []
 
 for file in listdir(path):
     #print(file)
-    df = pd.read_csv(path + "\\" + file, sep='\t', names=header, quoting=csv.QUOTE_NONE)
-    df1 = df[['stream_ID', 'game_name', 'broadcaster_id', 'broadcaster_name', 'follower_number', 'broadcaster_language']]
+    df = pd.read_csv(path + "\\" + file, sep='\t', header=None, names=header, quoting=csv.QUOTE_NONE, index_col=None)
+    df1 = df[['game_name', 'broadcaster_id', 'broadcaster_name', 'follower_number', 'broadcaster_language']]
     dfs.append(df1)
 
 df_all = pd.concat(dfs, ignore_index=True)
